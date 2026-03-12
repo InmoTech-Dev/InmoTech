00a3f89a4c83ded6473af40e02c625542a701d71
CHADI132 <dchalaracajramillo.com>
2026-03-12 10:18:48 -0500

    feat: add new route for image uploads with authentication and rate limiting.

--- api/src/routes/upload.routes.js
+++ api/src/routes/upload.routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { authenticateToken } = require('../middlewares/auth.middleware');
 const { uploadLimiter } = require('../middlewares/security.middleware');
 const { uploadSingleAny, subirImagen } = require('../controllers/upload.controller');
-const { uploadSingleAny, subirImagen } = require('../controllers/upload.controller');
 
 // Solo usuarios autenticados pueden subir su imagen
 router.post('/upload', authenticateToken, uploadLimiter, uploadSingleAny, subirImagen);
